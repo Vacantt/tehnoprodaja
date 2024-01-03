8d282c84258fa5b52c152af881a8c70ecb3bba4d
Marko Rufić <juongrudge3@gmail.com>
2024-01-03 20:20:20 +0100

    popup

--- custom-js/leaflet.js
+++ custom-js/leaflet.js
@@ -4,3 +4,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
+
+L.marker([44.804, 20.397]).addTo(map);
+L.popup().setLatLng([44.804, 20.397]).setContent("Tehnoprodaja prodavnica").openOn(map);
