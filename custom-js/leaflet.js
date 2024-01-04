231a146a242d4db7b9daba419eee3b33a83ff20d
Marko Rufić <juongrudge3@gmail.com>
2024-01-04 16:27:37 +0100

    Dodate klase, sklonjen style

--- custom-js/leaflet.js
+++ custom-js/leaflet.js
@@ -1,9 +1,8 @@
-var map = L.map('map').setView([44.804, 20.397], 16);
+var map = L.map('map').setView([44.80345, 20.39715], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
 
-L.marker([44.804, 20.397]).addTo(map);
-L.popup().setLatLng([44.804, 20.397]).setContent("Tehnoprodaja prodavnica").openOn(map);
+L.marker([44.80345, 20.39715]).addTo(map).bindPopup("Sedište Tehnoprodaja prodavnice:<br>Jurija Gagarina 31, Novi Beograd").openPopup();
